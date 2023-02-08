176e473dac38dd57d558caf922a4e8bacfb42e5c
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-08 21:59:32 +0100

    Zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var gender = "Kobieta";
-var age = 20;
-string name = "Ewa";
-
-if (gender == "kobieta" && age < 30)
+﻿int number = 19950412;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach (char letter in letters)
 {
+    if (letter == '0')
+    { counter0++; }
+    else if (letter == '1')
+    { counter1++; }
+    else if (letter == '2')
+    { counter2++; }
+    else if (letter == '3')
+    { counter3++; }
+    else if (letter == '4')
+    { counter4++; }
+    else if (letter == '5')
+    { counter5++; }
+    else if (letter == '6')
+    { counter6++; }
+    else if (letter == '7')
+    { counter7++; }
+    else if (letter == '8')
+    { counter8++; }
+    else if (letter == '9')
+    { counter9++; }
 
-    Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (name=="Ewa" && age > 18)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (age < 18 || gender != "kobieta")
-{
-    Console.WriteLine("Niepelnoletni mężczyzna");
 
-}
+
+Console.WriteLine("Moja liczba to: " + number);
+Console.WriteLine("0 ==> " + counter0);
+Console.WriteLine("1 ==> " + counter1);
+Console.WriteLine("2 ==> " + counter2);
+Console.WriteLine("3 ==> " + counter3);
+Console.WriteLine("4 ==> " + counter4);
+Console.WriteLine("5 ==> " + counter5);
+Console.WriteLine("6 ==> " + counter6);
+Console.WriteLine("7 ==> " + counter7);
+Console.WriteLine("8 ==> " + counter8);
+Console.WriteLine("9 ==> " + counter9);
