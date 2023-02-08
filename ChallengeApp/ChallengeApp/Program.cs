8e90dcb5d7c328250634182c0e9413167848cfdd
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-08 21:09:48 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var gender = "Kobieta";
+var age = 20;
+string name = "Ewa";
+
+if (gender == "kobieta" && age < 30)
+{
+
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name=="Ewa" && age > 18)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 || gender != "kobieta")
+{
+    Console.WriteLine("Niepelnoletni mężczyzna");
+
+}
