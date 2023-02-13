c44da6ec743264aa05475c967f2f022bdde62bf1
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-13 17:58:06 +0100

    Dzień 9

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,39 +1,11 @@
-﻿var User1 = new User("Wiktor", "Kwilosz", 27);
-User1.AddScore(5);
-User1.AddScore(6);
-User1.AddScore(9);
-User1.AddScore(7);
-User1.AddScore(4);
+﻿
 
-var User2 = new User("Jan", "Kowalski", 20);
-User2.AddScore(3);
-User2.AddScore(5);
-User2.AddScore(4);
-User2.AddScore(9);
-User2.AddScore(8);
+var employee = new Employee("Wiktor", "Kwilosz");
+employee.AddGrade(6);
+employee.AddGrade(7);
+employee.AddGrade(8);
+var statistics = employee.GetStatistics();
 
-var User3 = new User("Ewa", "Kowalska", 19);
-User3.AddScore(1);
-User3.AddScore(2);
-User3.AddScore(3);
-User3.AddScore(4);
-User3.AddScore(5);
-
-List<User> Users = new List<User>()
-{
-    User1, User2, User3
-};
-int MaxResult = -1;
-User UsersWithMaxResult = null;
-
-foreach (var user in Users)
-{
-    if (user.Result > MaxResult)
-    {
-        MaxResult = user.Result;
-        UsersWithMaxResult = user;
-    }
-
-}
-Console.WriteLine("Pracownik z najwyższą ilością punktów to:" + UsersWithMaxResult.Name + " " + UsersWithMaxResult.Surname + " " + "Lat" + UsersWithMaxResult.Age);
-Console.WriteLine("Suma punktów:" + UsersWithMaxResult.Result);
+Console.WriteLine($"Average: {statistics.Average}");
+Console.WriteLine($"Min:{statistics.Min}");
+Console.WriteLine($"Max:{statistics.Max}");
