6845f12cdc172feafe65444672f2171ced2c5e07
Wiktor <Wiktor@DESKTOP-CPOS1R4>
2023-02-15 22:21:04 +0100

    Zadanie domowe dzień 11

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,4 +1,7 @@
-﻿namespace ChallengeApp
+﻿using System;
+using System.Diagnostics;
+
+namespace ChallengeApp
 {
 
 
@@ -25,10 +28,7 @@
                     grades.Add(grade);
                 }
             
-            else
-            {
-                Console.WriteLine("invalid grade value");
-            }
+           
      
         }
         public void AddGrade(long grade)
@@ -52,10 +52,7 @@
             {
                 this.AddGrade(result);
             }
-            else
-            {
-                Console.WriteLine("String is not float");
-            }
+          
             
            
 
@@ -70,6 +67,28 @@
 
             foreach (var grade in grades)
             {
+                if(grade< 0)
+                {
+                    continue;
+                }
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+            statistics.Average = statistics.Average / grades.Count;
+
+            return statistics;
+        }
+        public Statistics GetStatisticsWithForEach()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                
                 statistics.Max = Math.Max(statistics.Max, grade);
                 statistics.Min = Math.Min(statistics.Min, grade);
                 statistics.Average += grade;
@@ -79,7 +98,65 @@
             return statistics;
 
         }
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
 
+            for (int index = 0; index < this.grades.Count; index++)
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Average += grades[index];
+            }
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            var index = 0;
+            do
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Average += grades[index];
+                index++;
+
+            } while (index < this.grades.Count);
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+        }
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            var index = 0;
+
+            while (index > this.grades.Count)
+            {
+                statistics.Max = Math.Max(statistics.Max, grades[index]);
+                statistics.Min = Math.Min(statistics.Min, grades[index]);
+                statistics.Average += grades[index];
+                index++;     
+            }
+            statistics.Average = statistics.Average / this.grades.Count;
+
+            return statistics;
+
+        }
+        
 
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -8,8 +8,28 @@ employee.AddGrade(7);
 employee.AddGrade(3l);
 employee.AddGrade(7f);
 employee.AddGrade(8);
-var statistics = employee.GetStatistics();
 
+var statistics = employee.GetStatisticsWithForEach();
+var statistics1 = employee.GetStatisticsWithFor();
+var statistics2 = employee.GetStatisticsWithDoWhile();
+var statistics3 = employee.GetStatisticsWithWhile();
+
+Console.WriteLine("Pętla For Each");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min:{statistics.Min}");
+Console.WriteLine($"Max:{statistics.Max}");
+
+Console.WriteLine("Pętla For");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min:{statistics.Min}");
+Console.WriteLine($"Max:{statistics.Max}");
+
+Console.WriteLine("Pętla Do While");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min:{statistics.Min}");
+Console.WriteLine($"Max:{statistics.Max}");
+
+Console.WriteLine("Pętla While");
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min:{statistics.Min}");
 Console.WriteLine($"Max:{statistics.Max}");
